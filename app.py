68ad273531cf257e618b928d43fff5335166bacf
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-01-15 15:18:16 +0000

    extending HTML templates
    using base.html to hold templates and using block and endblocks

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template               
 
 app = Flask(__name__)         
-@app.route('/')               # this is defining two routes that map to the same function (index()). if a user visits the root URL (/) or /index, the same content will be displayed.
+@app.route('/')               
 @app.route('/index')
 def index():                  
-    return render_template('index.html', current_title='Custom Title') # allows us to change out html dynamically. 
+    return render_template('index.html') 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)        
+    
+    
+    """
+    Whats happening:
+    
+    Now when we run app.py, it will run the index route. Inside the index.html, it will essentially load the base.html file. 
+    Inside this base.html file is the body text , the larger chunk/template of code. 
+    So itll display this body of code, and whatever the code remaining inside the index.html.
+    the larger chunk will display first as this is how it is strunctured inside the index.html fil.
+    Next, the about page will be rendered as this is the next (@app.route('/about')). 
+    Similarly, the base.html will be loaded and then followed by whatever is left inside the about.html file.
+    In the base.html file, at the bottom it has:
+    {%block main%}
+    {%endblock main%}
+    On the index.html and about.html files, whatre is between the block main and endblock, is what will display on each individual page alone with the code already on the base.html
+    """
