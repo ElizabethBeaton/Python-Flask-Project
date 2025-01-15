8d74dbda73e6acc1eccbfe7d5b1bcd7f1c77b4a1
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-01-15 12:21:29 +0000

    add files

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask                
+from flask import Flask, render_template               
 
-app = Flask(__name__)         # ' Flask(__name__) ' creates an instance of the Flask class. __name__ is a special variable in Python that contains the name of the current module. Flask uses this to locate resources like templates and static files.
-@app.route('/')               # route decor
-def hello():                  # defining a function
-    return "Hello, world!"
+app = Flask(__name__)         
+@app.route('/')               # this is defining two routes that map to the same function (index()). if a user visits the root URL (/) or /index, the same content will be displayed.
+@app.route('/index')
+def index():                  
+    return render_template('index.html', current_title='Custom Title') # allows us to change out html dynamically. 
 
 if __name__ == '__main__':
-    app.run(debug=True)        # starts the Flask development server to serve the application. debug=True enables debug mode: Automatically reloads the server when you make changes to the code and Displays detailed error messages in the browser if an error occurs.
+    app.run(debug=True)        
