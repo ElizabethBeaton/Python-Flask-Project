af548129809d9b5b617c79d590c366898fe2fa96
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-01-17 20:20:31 +0000

    Handling POST Requests

--- routes.py
+++ routes.py
@@ -8,7 +8,10 @@ import forms
 def index():                  
     return render_template('index.html') 
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        print('Submitted title', form.title.data) #accessing info on the forms
+        return render_template('about.html', form=form, title=form.title.data) #passing on multiple things to our template
     return render_template('about.html', form=form)
